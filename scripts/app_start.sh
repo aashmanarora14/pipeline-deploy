9a9f837136367a3c32e5e710228eb67689ba76db
aashmanarora14 <137777040+aashmanarora14@users.noreply.github.com>
2024-08-21 16:51:48 +0530

    Update app_start.sh

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,14 +1,2 @@
 #!/bin/bash
-
-# Navigate to the project directory
-cd /home/ubuntu/pipeline-deploy
-
-# Build the React application
 npm run build
-
-# Optionally, install `serve` globally if not installed
-npm install -g serve
-
-# Start serving the application
-serve -s build
-
